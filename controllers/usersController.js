3178f1b3730a8fc9c6779297a5786319b6f298a9
Shiao-Li <roberto.shiao@epn.edu.ec>
2023-11-23 20:57:43 -0500

    Crear usuarios

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -14,5 +14,24 @@ module.exports = {
                 message: 'Error al obtener los usuarios'
             });
         }
+    },
+    async register(req, res, next){
+        try {
+            const user = req.body;
+            const data = await User.create(user);
+
+            return res.status(201).json({
+                success: true,
+                message: 'El registro se realizo correctamente',
+                data: data.id
+            })
+        } catch (error) {
+            console.log(`Error: ${error}`);
+            return res.status(501).json({
+                success: false,
+                message: 'Error al registrar el usuario',
+                error: error
+            });
+        }
     }
 };
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,6 +1,8 @@
 const UsersController = require('../controllers/usersController');
 
 module.exports = (app) => {
-
+    //obteniendo los datos
     app.get('/api/users/getAll', UsersController.getAll);
+    //crear datos
+    app.post('/api/users/create', UsersController.register)
 }
