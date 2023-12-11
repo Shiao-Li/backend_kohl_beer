9bb09ae4ddf51f08ac1a81b2574efaa658ee7f13
Shiao-Li <roberto.shiao@epn.edu.ec>
2023-12-10 21:38:55 -0500

    deployd test

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -4,7 +4,7 @@ module.exports = (app, upload) => {
     //obteniendo los datos
     app.get('/api/users/getAll', UsersController.getAll);
     //crear datos, con imagen del usuario
-    app.post('/api/users/create', upload.array('image', 1), UsersController.registerWithImage);
+    app.post('/api/users/create', upload.array('image', 1), UsersController.register);
     //login
     app.post('/api/users/login', UsersController.login);
 }
