ad2fc9fa8b5926325c9c047a1ec18f70e9d924ea
Shiao-Li <roberto.shiao@epn.edu.ec>
2023-12-11 21:30:41 -0500

    fix test funcion resgistro con imagen

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -3,8 +3,11 @@ const UsersController = require('../controllers/usersController');
 module.exports = (app, upload) => {
     //obteniendo los datos
     app.get('/api/users/getAll', UsersController.getAll);
+    
     //crear datos, con imagen del usuario
+    // app.post('/api/users/create', upload.array('image', 1), UsersController.register);
     app.post('/api/users/create', upload.array('image', 1), UsersController.registerWithImage);
+
     //login
     app.post('/api/users/login', UsersController.login);
 }
