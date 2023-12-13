7a582918e6851a4b3786c93cec2505dc0ba16462
Shiao-Li <roberto.shiao@epn.edu.ec>
2023-12-13 00:12:50 -0500

    actualizar usuario completo

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -3,11 +3,19 @@ const UsersController = require('../controllers/usersController');
 module.exports = (app, upload) => {
     //obteniendo los datos
     app.get('/api/users/getAll', UsersController.getAll);
-    
+
+    // Usuario por id
+    app.get('/api/users/findById/:id', UsersController.findById);
+
     //crear datos, con imagen del usuario
-    // app.post('/api/users/create', upload.array('image', 1), UsersController.register);
     app.post('/api/users/create', upload.array('image', 1), UsersController.registerWithImage);
 
     //login
     app.post('/api/users/login', UsersController.login);
+
+    //logout
+    app.post('/api/users/logout', UsersController.logout);
+
+    //Actualizar datos
+    app.put('/api/users/update', upload.array('image', 1), UsersController.update)
 }
