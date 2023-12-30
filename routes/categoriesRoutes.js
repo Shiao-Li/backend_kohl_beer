a3ae258bf37795483e1bb95dae71f9ba4127b38d
Shiao-Li <roberto.shiao@epn.edu.ec>
2023-12-30 01:38:29 -0500

    pequeñas correcciones

--- routes/categoriesRoutes.js
+++ routes/categoriesRoutes.js
@@ -4,6 +4,7 @@ const passport = require('passport');
 module.exports = (app) => {
     // Obtener categorias
     app.get('/api/categories/getAll', passport.authenticate('jwt', { session: false }), CategoriesController.getAll);
+    
     // Crear categorias
     app.post('/api/categories/create', passport.authenticate('jwt', { session: false }), CategoriesController.create);
 }
